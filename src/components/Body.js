eac7cbd7f324b237e567026d2b160ce467a86a06
Sunil katkar <sunil.katkar@in.abb.com>
2023-01-09 15:36:43 +0530

    first commit

---
+++ src/components/Body.js
@@ -0,0 +1,12 @@
+const Body = () => {
+    return (
+      <div className="restaurant-list">
+        {restrautList.map((restaurant) => {
+          return <RestrauntCard {...restaurant.data} key={restaurant.data.id} />;
+        })}
+      </div>
+    );
+  };
+
+
+  export default Body
